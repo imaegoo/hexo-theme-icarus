87119f4e12d25c8e524a5b1025befa6a7c0af755
Jimmy Ma <jimmyma@augmentum.com.cn>
2019-05-28 19:51:01 +0800

    switch to loli cdn

--- includes/helpers/cdn.js
+++ includes/helpers/cdn.js
@@ -13,11 +13,13 @@ const cdn_providers = {
 };
 
 const font_providers = {
-    google: 'https://fonts.googleapis.com/${ type }?family=${ fontname }'
+    google: 'https://fonts.googleapis.com/${ type }?family=${ fontname }',
+    loli: 'https://fonts.loli.net/${ type }?family=${ fontname }'
 };
 
 const icon_providers = {
     bootcss: 'https://cdn.bootcss.com/font-awesome/5.4.1/css/all.css',
+    loli: 'https://cdnjs.loli.net/ajax/libs/font-awesome/5.4.1/css/all.css',
     fontawesome: 'https://use.fontawesome.com/releases/v5.4.1/css/all.css'
 };
 
